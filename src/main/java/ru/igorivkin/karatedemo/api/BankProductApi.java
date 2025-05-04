292359118b80afb82cacfda12f048e23a2e9bff5
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-05-04 11:59:40 +0530

    refactor

--- src/main/java/ru/igorivkin/karatedemo/api/BankProductApi.java
+++ src/main/java/ru/igorivkin/karatedemo/api/BankProductApi.java
@@ -4,24 +4,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.igorivkin.karatedemo.model.BankProduct;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/bankproducts")
 public class BankProductApi {
 
-    @GetMapping("/bankproducts/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<BankProduct> getBankProductById(@PathVariable Integer id) {
         return Optional.ofNullable(id)
                 .map(idNotNull -> {
                     if (idNotNull.equals(1)) {
-                        BankProduct bankProduct =
-                                BankProduct.builder()
-                                        .id(1)
-                                        .title("My product")
-                                        .build();
+                        BankProduct bankProduct = BankProduct.builder().id(1).title("My product").build();
                         return ResponseEntity.ok().body(bankProduct);
                     } else {
                         return ResponseEntity.status(HttpStatus.NOT_FOUND).<BankProduct>body(null);
