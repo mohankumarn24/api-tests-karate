f621dc480540e19aa5f9981fbe6a54d45d968407
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-10-08 09:30:25 +0530

    refactor

--- src/test/java/net/projectsync/karatedemo/repository/BankProductRepositoryTest.java
+++ src/test/java/net/projectsync/karatedemo/repository/BankProductRepositoryTest.java
@@ -7,12 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-
 import java.util.List;
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
